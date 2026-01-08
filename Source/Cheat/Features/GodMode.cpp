1cc963c2057fbfe3cbe0b7ff913e2117333e2a4c
Nerostav <Nerostav@DESKTOP-G7J503A>
2026-01-08 18:03:04 -0300

    Update Repository -> 08/01/2026 -> 18:02

--- Source/Cheat/Features/GodMode.cpp
+++ Source/Cheat/Features/GodMode.cpp
@@ -9,8 +9,8 @@ void GodMode()
 		if (!Ptr1) return;
 		uintptr_t Ptr2 = *(uintptr_t*)(Ptr1 + 0xC0);  
 		if (!Ptr2) return;
-		uintptr_t VehicleDamageAddress = (Ptr2 + 0x1F60);
+		uintptr_t DamageAddress = (Ptr2 + 0x1F60);
 
-		*(float*)VehicleDamageAddress = 0.0f;
+		*(float*)DamageAddress = 0.0f;
 	}
 }																	
--- Source/Cheat/Features/ResetTimer.cpp
+++ Source/Cheat/Features/ResetTimer.cpp
@@ -3,45 +3,24 @@
 
 void ResetTimer()
 {
-    static bool resetExecuted = false;
-
-    // ponteiros
     uintptr_t Ptr1 = *(uintptr_t*)(Client + 0x1279228);
     if (!Ptr1) return;
-
     uintptr_t Ptr2 = *(uintptr_t*)(Ptr1 + 0x20);
     if (!Ptr2) return;
-
     uintptr_t Ptr3 = *(uintptr_t*)(Ptr2 + 0x40);
     if (!Ptr3) return;
 
-    float* ResetTimer_Value = (float*)(Ptr3 + 0x0);
+    double* ResetTimer_Value = (double*)(Ptr3 + 0x0);
 
-
-    
-
-
-
-
-    // --------------------------------------------------
-    // RESET UMA VEZ (volta a subir)
-    // --------------------------------------------------
+    static bool resetExecuted = false;
     if (Config::ResetTimerOnce && !resetExecuted)
     {
-        *ResetTimer_Value = 0.0f;
+        *ResetTimer_Value = 0.0;
         resetExecuted = true;
     }
 
     if (!Config::ResetTimerOnce)
     {
         resetExecuted = false;
-    }
-
-    // --------------------------------------------------
-    // FREEZE TIMER (fica travado em 0)
-    // --------------------------------------------------
-    if (Config::FreezeTimer)
-    {
-        *ResetTimer_Value = 0.0f;
     }
 }
