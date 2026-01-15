2bf3d313f7fa446476d703a8da7700ddcf2ac5db
Nerostav <Nerostav@DESKTOP-G7J503A>
2026-01-15 10:47:42 -0300

    Update dllmain.cpp

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -12,61 +12,55 @@
 #include "../External/kiero/kiero.h"
 #include "Cheat/Menu/Menu.h"
 
+bool init;
+bool init_hook = false;
+bool unloading = false;
 
 typedef HRESULT(__stdcall* Present) (IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
 Present oPresent;
 
-bool init;
-bool init_hook = false;
-bool unloading = false;     
-
-uintptr_t Client = (uintptr_t)GetModuleHandle(L"NFS11Remastered.exe");
+uintptr_t Client = (uintptr_t)GetModuleHandle("NFS11Remastered.exe");
 
 HRESULT static __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
 {
-    if (unloading)
-        return oPresent(pSwapChain, SyncInterval, Flags);
-    if (!init)
-    {
-        if (Menu::Init(pSwapChain))
-            init = true;
-        return oPresent(pSwapChain, SyncInterval, Flags);
-    }
+	if (unloading)
+		return oPresent(pSwapChain, SyncInterval, Flags);
+	if (!init)
+	{
+		if (Menu::Init(pSwapChain))
+			init = true;
+		return oPresent(pSwapChain, SyncInterval, Flags);
+	}
 
-    Menu::StartRender();
-    Menu::Render();
+	Menu::StartRender();
+	Menu::Render();
 
-    Controller::Execute();
-    Menu::EndRender();
-    return oPresent(pSwapChain, SyncInterval, Flags);
-}
-
-void static Init()
-{
-
+	Controller::Execute();
+	Menu::EndRender();
+	return oPresent(pSwapChain, SyncInterval, Flags);
 }
 
 void static Unload(HMODULE hModule)
 {
-    unloading = true;
+	unloading = true;
 
-    Menu::Unload();
-    kiero::unbind(8);
+	Menu::Unload();
+	kiero::unbind(8);
 }
 
 DWORD static WINAPI HackThread(HMODULE hModule)
 {
-    AllocConsole();
-    FILE* f;
-    freopen_s(&f, "CONOUT$", "w", stdout);
+	AllocConsole();
+	FILE* f;
+	freopen_s(&f, "CONOUT$", "w", stdout);
 
-    CONSOLE_CURSOR_INFO Cursor = { 1 };
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Cursor);
+	CONSOLE_CURSOR_INFO Cursor = { 1 };
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Cursor);
 
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
 
-    std::wcout << R"(
+	std::wcout << R"(
                 _____   _______________________  _________________ 
                 ___  | / /__  ____/_  ___/__  / / /__  __ \__  __ \
                 __   |/ /__  /_   _____ \__  /_/ /__  /_/ /_  /_/ /
@@ -82,39 +76,37 @@ DWORD static WINAPI HackThread(HMODULE hModule)
  |  [!] This program is free software you can redistribute or modify it!          |
  +--------------------------------------------------------------------------------+   )";
 
-    while (!init_hook)
-    {
-        if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success)
-        {
-            Init();
-            kiero::bind(8, (void**)&oPresent, hkPresent);
-            init_hook = true;
-        }
-    }
+	while (!init_hook)
+	{
+		if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success)
+		{
+			kiero::bind(8, (void**)&oPresent, hkPresent);
+			init_hook = true;
+		}
+	}
 
-    while (!GetAsyncKeyState(VK_END))
-    {
-        Sleep(1);
-    }
+	while (!GetAsyncKeyState(VK_END))
+	{
+		Sleep(1);
+	}
 
-    Unload(hModule);
-    fclose(f);
-    FreeConsole();
-    FreeLibraryAndExitThread(hModule, EXIT_SUCCESS);
-    return 0;
+	Unload(hModule);
+	fclose(f);
+	FreeConsole();
+	FreeLibraryAndExitThread(hModule, EXIT_SUCCESS);
+	return 0;
 }
 
 BOOL static APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        DisableThreadLibraryCalls(hModule);
-        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
-        break;
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hModule);
+		CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
